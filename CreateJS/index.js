2b4bdb1b22e318adf60a00c76e2f6a1272ec727e
eadventurous <e.o.ivkov@gmail.com>
2019-10-22 21:12:25 +0300

    Moving ground

--- CreateJS/index.js
+++ CreateJS/index.js
@@ -1,27 +1,43 @@
 var stage, w, h, loader;
-var background;
+var background, ground;
 
-function init(){
+function init() {
     stage = new createjs.Stage("demoCanvas");
 
     w = stage.canvas.width;
     h = stage.canvas.height;
-    
+
     loader = new createjs.LoadQueue();
 
     manifest = [
-        {src: "background.png", id: "background"}
+        { src: "background.png", id: "background" },
+        { src: "groundGrass.png", id: "ground" }
     ]
 
     loader.addEventListener("complete", handleComplete);
     loader.loadManifest(manifest, true, "../assets/png/");
 }
 
-function handleComplete(){
+function handleComplete() {
     background = new createjs.Shape();
     backgroundImg = loader.getResult("background");
     background.graphics.beginBitmapFill(backgroundImg).drawRect(0, 0, w, h);
-    stage.addChild(background);
-    stage.update();
+
+    var groundImg = loader.getResult("ground");
+    ground = new createjs.Shape();
+    ground.graphics.beginBitmapFill(groundImg).drawRect(0, 0, w + groundImg.width, groundImg.height);
+    ground.tileW = groundImg.width;
+    ground.y = h - groundImg.height;
+
+    stage.addChild(background, ground);
+
+    createjs.Ticker.timingMode = createjs.Ticker.RAF;
+    createjs.Ticker.addEventListener("tick", tick);
+}
+
+function tick(event) {
+    var deltaS = event.delta / 1000;
+    ground.x = (ground.x - deltaS * 150) % ground.tileW;
+    stage.update(event);
 }
 
