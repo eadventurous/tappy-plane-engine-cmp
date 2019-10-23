e89a146be498175fdd71a0aecdf262399ec10b26
eadventurous <e.o.ivkov@gmail.com>
2019-10-23 18:52:21 +0300

    rock spawn added

--- CreateJS/index.js
+++ CreateJS/index.js
@@ -1,6 +1,12 @@
 var stage, w, h, loader;
 var background, ground, upperground;
 
+var rocks = [];
+var rockSpawnTime = 1500;
+var rockSpawnTimeVariation = 700;
+var rockSpawnCountdown = rockSpawnTime;
+var rockType = false;
+
 function init() {
     stage = new createjs.Stage("demoCanvas");
 
@@ -11,11 +17,14 @@ function init() {
 
     manifest = [
         { src: "background.png", id: "background" },
-        { src: "groundGrass.png", id: "ground" }
+        { src: "groundGrass.png", id: "ground" },
+        { src: "rockGrass.png", id: "rock"},
+        { src: "rockGrassDown.png", id: "rockDown"}
     ]
 
     loader.addEventListener("complete", handleComplete);
     loader.loadManifest(manifest, true, "../assets/png/");
+    
 }
 
 function handleComplete() {
@@ -46,11 +55,39 @@ function shiftGroundX(ground, deltaX){
     return (ground.x - deltaX) % ground.tileW;
 }
 
+function spawnRock(down){
+    let rockImg = loader.getResult(down ? "rockDown" : "rock");
+    let rock = new createjs.Shape();
+    rock.graphics.beginBitmapFill(rockImg).drawRect(0, 0, rockImg.width, rockImg.height);
+    rock.x = w;
+    rock.y = down ? 0 : h - rockImg.height;
+    rock.width = rockImg.width;
+    rocks.push(rock);
+    stage.addChildAt(rock, stage.getChildIndex(background)+1);
+}
+
 function tick(event) {
     let deltaS = event.delta / 1000;
+
+    rockSpawnCountdown -= event.delta;
+    if(rockSpawnCountdown < 0) {
+        spawnRock(rockType);
+        rockType = !rockType;
+        rockSpawnCountdown = rockSpawnTime + Math.random()*rockSpawnTimeVariation;
+    }
+
     let speed = 150;
-    ground.x = shiftGroundX(ground, deltaS*speed);
-    upperground.x = shiftGroundX(upperground, deltaS*speed);
+    shift = deltaS*speed;
+
+    ground.x = shiftGroundX(ground, shift);
+    upperground.x = shiftGroundX(upperground, shift);
+
+    rocks.forEach(rock => {
+        rock.x -= shift;
+        if(rock.x <= -rock.width) stage.removeChild(rock);
+    });
+    rocks = rocks.filter(rock => rock.x > -rock.width);
+
     stage.update(event);
 }
 
