e57ac8879107f3826a1611708404368ee45129bb
eadventurous <e.o.ivkov@gmail.com>
2019-11-09 18:35:22 +0300

    show background

--- MelonJS/js/game.js
+++ MelonJS/js/game.js
@@ -12,7 +12,7 @@ var game = {
     // Run on page load.
     "onload" : function () {
         // Initialize the video.
-        if (!me.video.init(960, 640, {wrapper : "screen", scale : "auto"})) {
+        if (!me.video.init(800, 480, {wrapper : "screen", scale : "auto"})) {
             alert("Your browser does not support HTML5 canvas.");
             return;
         }
@@ -27,11 +27,10 @@ var game = {
 
     // Run on game resources loaded.
     "loaded" : function () {
-        me.state.set(me.state.MENU, new game.TitleScreen());
         me.state.set(me.state.PLAY, new game.PlayScreen());
 
         // add our player entity in the entity pool
-        me.pool.register("mainPlayer", game.PlayerEntity);
+        //me.pool.register("planeObj", game.PlayerEntity);
 
         // Start the game.
         me.state.change(me.state.PLAY);
