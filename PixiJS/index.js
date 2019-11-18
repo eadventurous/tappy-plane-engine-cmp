d0c73815a2bbc9ee5d62dba4d02f5635f1998923
eadventurous <e.o.ivkov@gmail.com>
2019-11-18 19:41:07 +0300

    moving ground

--- PixiJS/index.js
+++ PixiJS/index.js
@@ -1,9 +1,19 @@
 var gameWidth = 800;
 var gameHeight = 460;
 
+var state;
+var ticker;
+
 var ground;
+
+var speed = 150;
 //Create a Pixi Application
-let app = new PIXI.Application({ width: gameWidth, height: gameHeight, antialias: true });
+let app = new PIXI.Application({
+     width: gameWidth, 
+     height: gameHeight, 
+     antialias: true,
+     sharedTicker: true, 
+    });
 
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
@@ -22,20 +32,41 @@ function setup() {
 
     let groundTex = resources.ground.texture;
     let tileW = groundTex.width;
-    ground = new PIXI.TilingSprite(groundTex,gameWidth + tileW, groundTex.height);
+    ground = new PIXI.TilingSprite(groundTex, gameWidth + tileW, groundTex.height);
     ground.tileW = tileW;
     ground.x = shiftGroundX(ground, Math.random() * tileW);
     ground.y = gameHeight - groundTex.height;
     app.stage.addChild(ground);
 
-    upperground = new PIXI.TilingSprite(groundTex,gameWidth + tileW, groundTex.height);
+    upperground = new PIXI.TilingSprite(groundTex, gameWidth + tileW, groundTex.height);
     upperground.tileW = tileW;
     upperground.x = shiftGroundX(upperground, Math.random() * tileW);
     upperground.y = groundTex.height;
     upperground.scale.y = -1;
     app.stage.addChild(upperground);
+
+    state = play;
+
+    ticker = PIXI.Ticker.shared;
+    ticker.add(delta => gameLoop(delta));
+    ticker.start();
 }
 
-function shiftGroundX(ground, deltaX){
+function gameLoop(delta) {
+
+    //Update the current game state:
+    state(delta);
+}
+
+function play(delta) {
+    let deltaS = ticker.elapsedMS / 1000;
+
+    shift = deltaS*speed;
+
+    ground.x = shiftGroundX(ground, shift);
+    upperground.x = shiftGroundX(upperground, shift);
+}
+
+function shiftGroundX(ground, deltaX) {
     return (ground.x - deltaX) % ground.tileW;
 }
