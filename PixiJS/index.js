22cee805c47683423843fa70ba88c516048e535d
eadventurous <e.o.ivkov@gmail.com>
2019-11-18 19:28:00 +0300

    Added ground

--- PixiJS/index.js
+++ PixiJS/index.js
@@ -1,10 +1,12 @@
+var gameWidth = 800;
+var gameHeight = 460;
+
+var ground;
 //Create a Pixi Application
-let app = new PIXI.Application({ width: 900, height: 460, antialias: true });
+let app = new PIXI.Application({ width: gameWidth, height: gameHeight, antialias: true });
 
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
-
-PIXI.settings.WRAP_MODE = PIXI.WRAP_MODES.REPEAT;
 
 PIXI.loader
     .add("background", "../Assets/PNG/background.png")
@@ -15,10 +17,25 @@ function setup() {
 
     let resources = PIXI.loader.resources;
 
-    let backgroundTex = resources.background.texture;
-    backgroundTex.trim = new PIXI.Rectangle(0,0,900, 460);
-    backgroundTex.updateUvs();
-
-    let background = new PIXI.Sprite(backgroundTex);
+    let background = new PIXI.Sprite(resources.background.texture);
     app.stage.addChild(background);
+
+    let groundTex = resources.ground.texture;
+    let tileW = groundTex.width;
+    ground = new PIXI.TilingSprite(groundTex,gameWidth + tileW, groundTex.height);
+    ground.tileW = tileW;
+    ground.x = shiftGroundX(ground, Math.random() * tileW);
+    ground.y = gameHeight - groundTex.height;
+    app.stage.addChild(ground);
+
+    upperground = new PIXI.TilingSprite(groundTex,gameWidth + tileW, groundTex.height);
+    upperground.tileW = tileW;
+    upperground.x = shiftGroundX(upperground, Math.random() * tileW);
+    upperground.y = groundTex.height;
+    upperground.scale.y = -1;
+    app.stage.addChild(upperground);
+}
+
+function shiftGroundX(ground, deltaX){
+    return (ground.x - deltaX) % ground.tileW;
 }
