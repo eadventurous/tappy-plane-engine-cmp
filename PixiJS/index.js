dc591872c992808a36448f10a84ef3e6b3a2b92b
eadventurous <e.o.ivkov@gmail.com>
2019-11-18 14:09:38 +0300

    set up texture tiling

--- PixiJS/index.js
+++ PixiJS/index.js
@@ -1,20 +1,24 @@
 //Create a Pixi Application
-let app = new PIXI.Application({ width: 800, height: 460, antialias: true });
+let app = new PIXI.Application({ width: 900, height: 460, antialias: true });
 
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
 
+PIXI.settings.WRAP_MODE = PIXI.WRAP_MODES.REPEAT;
+
 PIXI.loader
     .add("background", "../Assets/PNG/background.png")
+    .add("ground", "../Assets/PNG/groundGrass.png")
     .load(setup);
 
 function setup() {
 
     let resources = PIXI.loader.resources;
 
-    //Create the cat sprite
-    let background = new PIXI.Sprite(resources.background.texture);
+    let backgroundTex = resources.background.texture;
+    backgroundTex.trim = new PIXI.Rectangle(0,0,900, 460);
+    backgroundTex.updateUvs();
 
-    //Add the cat to the stage
+    let background = new PIXI.Sprite(backgroundTex);
     app.stage.addChild(background);
 }
