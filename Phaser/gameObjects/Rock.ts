a5a2aa6aac58f3dca02a3ae56e2036cba9274467
eadventurous <e.o.ivkov@gmail.com>
2019-12-21 14:18:45 +0100

    rock class

---
+++ Phaser/gameObjects/Rock.ts
@@ -0,0 +1,34 @@
+import { Physics } from "phaser";
+
+export class Rock extends Phaser.GameObjects.Sprite {
+    private _upper = false;
+    public scrollSpeed: number;
+
+    get upper(): boolean {
+        return this._upper;
+    }
+
+    set upper(value: boolean) {
+        this._upper = value;
+        this.y = value ? this.height / 2 : this.scene.cameras.main.height - this.height / 2;
+        this.setRotation(Math.PI);
+    }
+
+    constructor(config) {
+        super(config.scene, 0, 0, "rock");
+        this.upper = config.upper;
+        this.scrollSpeed = config.scrollSpeed;
+        this.x = this.scene.cameras.main.width; //+ this.width / 2;
+        (config.scene as Phaser.Scene).add.existing(this);
+        (config.scene as Phaser.Scene).physics.add.existing(this);
+        (this.body as Physics.Arcade.Body).setAllowGravity(false)
+            .setSize(this.width / 2, this.height);
+    }
+
+    update(time: number, delta: number) {
+        this.x -= this.scrollSpeed * delta;
+        if (this.x <= -this.width) {
+            this.destroy();
+        }
+    }
+}
